4bfc7ae3f8d69c4151ebd22891181d6803f797be
Kiang <kiangtengl@gmail.com>
2014-10-13 18:35:47 +0800

    In the middle of adding support for browserify

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+
+gulp.task('js', function() {
+    gulp.src('src/js/main.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('./'))
+});
---
+++ src/ts/main.js
@@ -0,0 +1,1 @@
+﻿//# sourceMappingURL=main.js.map
